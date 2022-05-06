5ba5a918609e9e736ee37b1fab7c19f7f4485f22
DenisKurek <denis4kurek@gmail.com>
2022-05-06 22:49:05 +0200

    20.Specular Lighting

--- Labirynth/fragment_core.glsl
+++ Labirynth/fragment_core.glsl
@@ -10,7 +10,7 @@ out vec4 fs_color;
 uniform sampler2D texture0;
 
 uniform vec3 lightPos0;
-
+uniform vec3 cameraPos;
 void main() {
 	//fs_color = vec4(vs_color, 1.f);
 
@@ -23,8 +23,15 @@ void main() {
 	float diffuse =clamp(dot(posToLightDirVec, vs_normal),0,1);
 	vec3 diffuseFinal = diffuseColor * diffuse;
 
+	//Specular light
+	vec3 lightToPosDirVec = normalize(lightPos0 - vs_position);
+	vec3 reflectDirVec = normalize(reflect(lightToPosDirVec, normalize(vs_normal)));
+	vec3 posToViewDirVec = normalize(vs_position - cameraPos);
+	float specularConstant = pow(max(dot(posToViewDirVec, reflectDirVec), 0), 30);
+	vec3 specularFinal = vec3(1.f, 1.f, 1.f) * specularConstant;
+
 
 	fs_color =
 		texture(texture0, vs_texcoord) * vec4(vs_color, 1.f)
-		* (vec4(ambientLight, 1.f)+ vec4(diffuseFinal,1.f));
+		* (vec4(ambientLight, 1.f)+ vec4(diffuseFinal,1.f)+ vec4(specularFinal, 1.f));
 }
--- Labirynth/main.cpp
+++ Labirynth/main.cpp
@@ -279,7 +279,7 @@ int main() {
 	);
 
 	//LIGHTS
-	glm::vec3 lightPos0(0.f, 0.f, 2.f);
+	glm::vec3 lightPos0(0.f, 0.f, 1.f);
 
 	//INIT UNIFORMS
 	glUseProgram(core_program);
@@ -289,6 +289,7 @@ int main() {
 	glUniformMatrix4fv(glGetUniformLocation(core_program,"ProjectionMatrix"),1,GL_FALSE,glm::value_ptr(ProjectionMatrix));
 
 	glUniform3fv(glGetUniformLocation(core_program, "lightPos0"), 1, glm::value_ptr(lightPos0));
+	glUniform3fv(glGetUniformLocation(core_program, "cameraPos"), 1, glm::value_ptr(camPosition));
 
 	glUseProgram(0);
 	//MAIN LOOP
