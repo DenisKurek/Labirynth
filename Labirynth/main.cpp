0ba2585549b021526cc5abb8911ffead25ec4b72
DenisKurek <denis4kurek@gmail.com>
2022-05-06 21:48:56 +0200

    17.View_and_Projection_Matrix

--- Labirynth/main.cpp
+++ Labirynth/main.cpp
@@ -128,8 +128,8 @@ int main() {
 
 	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH,WINDOW_HEIGHT,"LABIRYNTH",NULL,NULL);
 
+	glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
 	glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
-	//glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
 	//glViewport(0, 0, framebufferWidth, framebufferHeight);
 
 	glfwMakeContextCurrent(window); //important for glew
@@ -217,7 +217,7 @@ int main() {
 	glBindTexture(GL_TEXTURE_2D, 0);
 	SOIL_free_image_data(image); //remove data (important)!!
 
-	//works right to left
+	//INIT MATRICIES works right to left
 	glm::mat4 ModelMatrix(1.f);
 	ModelMatrix = glm::translate(ModelMatrix,glm::vec3(0.f,0.f,0.f));
 	ModelMatrix = glm::rotate(ModelMatrix,glm::radians(0.f),glm::vec3(1.f,0.f,0.f));
@@ -225,9 +225,28 @@ int main() {
 	ModelMatrix = glm::rotate(ModelMatrix,glm::radians(0.f),glm::vec3(0.f,0.f,1.f));
 	ModelMatrix = glm::scale(ModelMatrix,glm::vec3(1.f));
 
+	glm::vec3 camPosition(0.f,0.f,1.f);
+	glm::vec3 worldUp(0.f,1.f,0.f);
+	glm::vec3 camFront(0.f,0.f,-1.f);
+	glm::mat4 ViewMatrix(1.f); //its important to initialize it
+	ViewMatrix = glm::lookAt(camPosition, camPosition + camFront, worldUp);
+
+	float fov = 90.f;
+	float nearPlane = 0.1f;
+	float farPlane = 100.f;
+	glm::mat4 ProjectionMatrix(1.f);
+	ProjectionMatrix = glm::perspective(
+		glm::radians(fov),
+		static_cast<float>(framebufferWidth)/framebufferHeight,
+		nearPlane,
+		farPlane
+	);
+	//INIT UNIFORMS
 	glUseProgram(core_program);
 
 	glUniformMatrix4fv(glGetUniformLocation(core_program,"ModelMatrix"),1,GL_FALSE,glm::value_ptr(ModelMatrix));
+	glUniformMatrix4fv(glGetUniformLocation(core_program,"ViewMatrix"),1,GL_FALSE,glm::value_ptr(ViewMatrix));
+	glUniformMatrix4fv(glGetUniformLocation(core_program,"ProjectionMatrix"),1,GL_FALSE,glm::value_ptr(ProjectionMatrix));
 
 	glUseProgram(0);
 	//MAIN LOOP
@@ -249,14 +268,23 @@ int main() {
 		glUniform1i(glGetUniformLocation(core_program, "texture0"), 0);
 
 		//move rotate and scale
-
-		ModelMatrix = glm::translate(ModelMatrix, glm::vec3(0.1f, 0.f, 0.f));
-		ModelMatrix = glm::rotate(ModelMatrix, glm::radians(0.f), glm::vec3(1.f, 0.f, 0.f));
-		ModelMatrix = glm::rotate(ModelMatrix, glm::radians(0.f), glm::vec3(0.f, 1.f, 0.f));
+		ModelMatrix = glm::translate(ModelMatrix, glm::vec3(0.f, 0.f, 0.f));
+		ModelMatrix = glm::rotate(ModelMatrix, glm::radians(2.f), glm::vec3(1.f, 0.f, 0.f));
+		ModelMatrix = glm::rotate(ModelMatrix, glm::radians(1.f), glm::vec3(0.f, 1.f, 0.f));
 		ModelMatrix = glm::rotate(ModelMatrix, glm::radians(0.f), glm::vec3(0.f, 0.f, 1.f));
 		ModelMatrix = glm::scale(ModelMatrix, glm::vec3(1.f));
-
 		glUniformMatrix4fv(glGetUniformLocation(core_program, "ModelMatrix"), 1, GL_FALSE, glm::value_ptr(ModelMatrix));
+		
+		glfwGetFramebufferSize(window,&framebufferWidth,&framebufferHeight);
+		ProjectionMatrix = glm::mat4(1.f);
+		ProjectionMatrix = glm::perspective(
+			glm::radians(fov),
+			static_cast<float>(framebufferWidth) / framebufferHeight,
+			nearPlane,
+			farPlane
+		);
+
+		glUniformMatrix4fv(glGetUniformLocation(core_program, "ProjectionMatrix"), 1, GL_FALSE, glm::value_ptr(ProjectionMatrix));
 
 		//Activate texture
 		glActiveTexture(GL_TEXTURE0);
--- Labirynth/vertex_core.glsl
+++ Labirynth/vertex_core.glsl
@@ -9,11 +9,13 @@ out vec3 vs_color;
 out vec2 vs_texcoord;
 
 uniform mat4 ModelMatrix;
+uniform mat4 ViewMatrix;
+uniform mat4 ProjectionMatrix;
 
 void main() {
 	vs_position =vec4(ModelMatrix*vec4(vertex_position,1.f)).xyz; //xyz (take onle 3 coordinates //sfizling)
 	vs_color = vertex_color;
 	vs_texcoord = vec2(vertex_texcoord.x, vertex_texcoord.y * -1.f);
 
-	gl_Position = ModelMatrix * vec4(vertex_position, 1.f);
+	gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(vertex_position, 1.f); //not modify vs_position by ProjectionMatrix
 }
